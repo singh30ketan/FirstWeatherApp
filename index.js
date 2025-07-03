508d06b6a47ec3228621890a2961210b65355f0a
tiketketansingh <ketan.singh@tiket.com>
2025-07-03 20:19:48 +0530

    fixed major image issues

--- index.js
+++ index.js
@@ -21,19 +21,19 @@ async function checkWeather(city){
         document.querySelector(".wind").innerHTML=data.wind.speed +" km/hr";
 
         if(data.weather[0].main == "Clouds"){
-            weatherIcon.src="images/clouds.png";
+            weatherIcon.src="Images/clouds.png";
         }
         else if(data.weather[0].main == "Clear"){
-            weatherIcon.src="images/clear.png";
+            weatherIcon.src="Images/clear.png";
         }
         else if(data.weather[0].main == "Rain"){
-            weatherIcon.src="images/rain.png";
+            weatherIcon.src="Images/rain.png";
         }
         else if(data.weather[0].main == "Drizzle"){
-            weatherIcon.src="images/drizzle.png";
+            weatherIcon.src="Images/drizzle.png";
         }
         else if(data.weather[0].main == "Mist"){
-            weatherIcon.src="images/mist.png";
+            weatherIcon.src="Images/mist.png";
         }
 
         document.querySelector(".weather").style.display="block";
